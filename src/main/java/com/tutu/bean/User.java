9fe94b52b1555f824b377d6b256c455dace5652a
root <1059894903@qq.com>
2020-05-19 14:36:15 +0800

    first commit

---
+++ src/main/java/com/tutu/bean/User.java
@@ -0,0 +1,55 @@
+package com.tutu.bean;
+
+import java.io.Serializable;
+
+/**
+ * @author tutu 2020/5/19 13:31
+ */
+public class User implements Serializable {
+    private Long id;
+    private String username;
+    private String gender;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
