aaa34570c58014ca10a84d294b9d23cdc8d7ea94
root <1059894903@qq.com>
2020-05-19 14:41:18 +0800

    first commit

--- src/main/java/com/tutu/utils/RedisUtil.java
+++ src/main/java/com/tutu/utils/RedisUtil.java
@@ -1,10 +1,8 @@
 package com.tutu.utils;
 
 import com.sun.istack.internal.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
